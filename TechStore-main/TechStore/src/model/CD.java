03f364a4f9892849aceb00ffbe04ca122ed503cd
IvaHoxha02 <161184905+IvaHoxha02@users.noreply.github.com>
2024-02-25 09:21:09 +0100

    Update CD.java

--- TechStore-main/TechStore/src/model/CD.java
+++ TechStore-main/TechStore/src/model/CD.java
@@ -1,25 +1,26 @@
 package model;
 
+import org.bson.Document;
+
 import java.io.Serializable;
 
-public class CD implements Serializable{
+public class CD implements Serializable {
     private static final long serialVersionUID = -5083759422249745403L;
     private String cdName;
     private int cdYear;
     private int cdId;
-   public static int x=0;
+    public static int x = 0;
     private int cdQuantity;
     private double cdPrice;
     private String cdCategory;
     private String cdAuthor;
     private long cdUPC;
 
-
     public CD() {
-        this("","",0,0,0,0,"",x++);
+        this("", "", 0, 0, 0, 0, "", x++);
     }
 
-    public CD(String cdName,String cdCategory,double cdPrice,int Year,int cdQuantity,long cdUPC,String cdAuthor,int id){
+    public CD(String cdName, String cdCategory, double cdPrice, int Year, int cdQuantity, long cdUPC, String cdAuthor, int id) {
         super();
         this.cdId = id;
         this.cdAuthor = cdAuthor;
@@ -31,72 +32,30 @@ public class CD implements Serializable{
         this.cdUPC = cdUPC;
     }
 
+    // Getters and setters for existing fields
 
-
-    public String getCdName(){
-        return cdName;
+    public Document toDocument() {
+        return new Document("cdName", cdName)
+                .append("cdCategory", cdCategory)
+                .append("cdPrice", cdPrice)
+                .append("cdYear", cdYear)
+                .append("cdQuantity", cdQuantity)
+                .append("cdUPC", cdUPC)
+                .append("cdAuthor", cdAuthor)
+                .append("cdId", cdId);
     }
 
-    public String getCdAuthor(){
-        return cdAuthor;
+    @Override
+    public String toString() {
+        return "CD{" +
+                "cdName='" + cdName + '\'' +
+                ", cdYear=" + cdYear +
+                ", cdId=" + cdId +
+                ", cdQuantity=" + cdQuantity +
+                ", cdPrice=" + cdPrice +
+                ", cdCategory='" + cdCategory + '\'' +
+                ", cdAuthor='" + cdAuthor + '\'' +
+                ", cdUPC=" + cdUPC +
+                '}';
     }
-
-    public String getCdCategory(){
-        return cdCategory;
-    }
-
-    public int getCdId(){
-        return cdId;
-    }
-
-    public double getCdPrice(){
-        return cdPrice;
-    }
-    public int getCdQuantity(){
-        return cdQuantity;
-    }
-    public int getCdYear(){
-        return cdYear;
-    }
-   public long getCdUPC(){return  cdUPC;}
-
-    public void setCdName(String name){
-        this.cdName = name;
-    }
-
-    public void setcdId(int id){
-        this.cdId = id;
-    }
-   public void setCdAuthor(String name){
-        this.cdAuthor = name;
-    }
-
-    public void setCdYear(int Year){
-        this.cdYear = Year;
-    }
-
-    public void setCdUPC(long upc){
-        this.cdUPC = upc;
-    }
-
-    public void setCdQuantity(int quantity) {
-	    if (quantity < 0) {
-	        throw new IllegalArgumentException("Quantity must be a positive number");
-	    }else {
-	    this.cdQuantity = quantity;}
-	    
-	}
-
-    
-    public void setCdPrice(double price) {
-        if (price < 0) {
-            throw new IllegalArgumentException("Price must be a positive number");
-        }else {
-        this.cdPrice = price;}
-    }
-
-    public void setCdCategory(String cat){
-        this.cdCategory = cat;
-    }
-
 }
