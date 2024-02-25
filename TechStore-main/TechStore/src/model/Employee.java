58f395547bdfe01c5c5a7507547becd47ad717d8
IvaHoxha02 <161184905+IvaHoxha02@users.noreply.github.com>
2024-02-25 09:21:45 +0100

    Update Employee.java

--- TechStore-main/TechStore/src/model/Employee.java
+++ TechStore-main/TechStore/src/model/Employee.java
@@ -1,41 +1,68 @@
 package model;
 
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.codecs.pojo.annotations.BsonIgnoreIfNull;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
 import java.io.Serializable;
 
 public class Employee implements Serializable {
     private static final long serialVersionUID = -5083759422249745403L;
+
+    @BsonId
+    private ObjectId _id;
+
+    @BsonProperty(value = "employeeName")
     private String employeeName;
+
+    @BsonProperty(value = "employeeSurname")
     private String employeeSurname;
-    private double employeeSalary;
 
+    @BsonProperty(value = "employeeSalary")
+    @BsonIgnoreIfNull
+    private Double employeeSalary;
 
-    public Employee(){this("","",0);}
+    public Employee() {
+        this("", "", 0.0);
+    }
 
-    public Employee(String name,String surname,double salary){
+    public Employee(String name, String surname, double salary) {
         super();
         this.employeeName = name;
         this.employeeSurname = surname;
         this.employeeSalary = salary;
     }
 
-    public String getEmployeeName(){
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public void set_id(ObjectId _id) {
+        this._id = _id;
+    }
+
+    public String getEmployeeName() {
         return employeeName;
     }
-    public String getEmployeeSurname(){
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeSurname() {
         return employeeSurname;
     }
 
-    public double getEmployeeSalary(){
-        return employeeSalary;
-    }
-    public void setEmployeeName(String name){
-        this.employeeName = name;
-    }
-    public void setEmployeeSurname(String surname){
-        this.employeeSurname = surname;
-    }
-    public void setEmployeeSalary(double salary){
-        this.employeeSalary = salary;
+    public void setEmployeeSurname(String employeeSurname) {
+        this.employeeSurname = employeeSurname;
     }
 
+    public Double getEmployeeSalary() {
+        return employeeSalary;
+    }
+
+    public void setEmployeeSalary(Double employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
 }
